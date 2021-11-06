a6d407872911a0336344765f1955a86635dd4f0f
Martin Aguirre <aguirremartinignacio@gmail.com>
2021-11-06 01:39:47 -0300

    estilos y funcion resumen()

--- TP-INTEGRADOR/js/script.js
+++ TP-INTEGRADOR/js/script.js
@@ -1,38 +1,24 @@
-var cantidad = parseInt(document.getElementById("cantidad").value);
-var categoria = parseInt(document.getElementById("categoria").value);
-var btnBorrar = document.getElementById("resumen");
-// var precioPagar = document.getElementById("precioPagar");
+let precio = parseInt(document.getElementById("precio").innerText);
+let btnBorrar = document.getElementById("borrar");
+let btnResumen = document.getElementById("resumen");
 
-btnBorrar.addEventListener("click", () => {
-  var total = 200 * cantidad;
-  var descuento = (total * categoria) / 100;
-  document.getElementById("precioPagar").innerText = total - descuento;
+function resumen(precio, cantidad, categoria) {
+  let total = precio * cantidad;
+  let descuento = (total * categoria) / 100;
+  return total - descuento;
+}
+
+btnResumen.addEventListener("click", () => {
+  let cantidad = parseInt(document.getElementById("cantidad").value);
+  let categoria = parseInt(document.getElementById("categoria").value);
+  document.getElementById("precioPagar").innerText = resumen(
+    precio,
+    cantidad,
+    categoria
+  );
 });
 
-function descuento() {
-  let t = 200 * cantidad;
-  let d = (t * categoria) / 100;
-  return d;
-}
-
-function resumen() {
-  document.getElementById("precioPagar").innerHTML = descuento();
-}
-
-// function alert(message, type) {
-//   var wrapper = document.createElement("div");
-//   wrapper.innerHTML =
-//     '<div class="alert alert-' +
-//     type +
-//     ' alert-dismissible" role="alert">' +
-//     message +
-//     '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>';
-
-//   alertPlaceholder.append(wrapper);
-// }
-
-// if (alertTrigger) {
-//   alertTrigger.addEventListener("click", function () {
-//     alert("Nice, you triggered this alert message!", "success");
-//   });
-// }
+btnBorrar.addEventListener("click", () => {
+  document.getElementById("cantidad").value = 0;
+  document.getElementById("precioPagar").innerText = 0;
+});
